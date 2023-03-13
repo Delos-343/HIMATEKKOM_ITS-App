beaf387f00a794f4392d941d6c27e295801c09d9
Delos-343 <mfachry.handoko@gmail.com>
2023-03-14 04:05:57 +0700

    renamed ui-ux asset folder

--- screens/TracklistScreen.js
+++ screens/TracklistScreen.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import LinearGradient from 'react-native-linear-gradient'
 
 function TracklistScreen() {
   return (
-    <div>
-        
-    </div>
+    <>
+        <LinearGradient
+            colors={['#0000ff', '#00005f', '#191414']}
+            style={styles.linearGradient}>
+            <Image
+            style={{width: 200, height: 200}}
+            source={{uri: 'https://www.bensound.com/bensound-img/punky.jpg'}}
+            />
+        </LinearGradient>
+    </>
   )
 }
 
