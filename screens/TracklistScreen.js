0821353b162b805e80b9727fcfd45b340e04eb26
Delos-343 <mfachry.handoko@gmail.com>
2023-03-14 04:20:01 +0700

    unit testing complete

--- screens/TracklistScreen.js
+++ screens/TracklistScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Image, Text, TouchableOpacity } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
 
 function TracklistScreen() {
@@ -8,12 +9,21 @@ function TracklistScreen() {
             colors={['#0000ff', '#00005f', '#191414']}
             style={styles.linearGradient}>
             <Image
-            style={{width: 200, height: 200}}
-            source={{uri: 'https://www.bensound.com/bensound-img/punky.jpg'}}
+                style={{width: 200, height: 200}}
+                source={{uri: '../assets/logos/Logo_HimaTekkom-ITS.png'}}
             />
         </LinearGradient>
+        <TouchableOpacity>
+            <Text style={[styles.helloWorld]}> Hello World </Text>
+        </TouchableOpacity>
     </>
   )
 }
+
+const styles = StyleSheet.create({
+    helloWorld: {
+      alignText: 'center',
+    },
+  });
 
 export default TracklistScreen
