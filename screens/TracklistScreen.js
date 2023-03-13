3d389707700bf8d6c035b43f97c99efd468d5db8
Delos-343 <mfachry.handoko@gmail.com>
2023-03-14 03:54:57 +0700

    added new [main] screen

---
+++ screens/TracklistScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TracklistScreen() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default TracklistScreen
---
+++ screens/index.js
@@ -0,0 +1,1 @@
+export { default as TracklistScreen } from './TracklistScreen'
