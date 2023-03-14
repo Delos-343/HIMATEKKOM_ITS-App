2871ff1df28a1dbf4ccbd8afb54fd7e4b242ae4d
Delos-343 <mfachry.handoko@gmail.com>
2023-03-14 07:41:19 +0700

    added new [main] screen

---
+++ screens/RadioPlayer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Animated, FlatList, SafeAreaView } from 'react-native'
+
+const RadioPlayer = () => {
+  return (
+    <SafeAreaView style={style.container}>
+        <View style={style.mainContainer}>
+            <Animated.FlatList
+                ref={songSlider}
+                renderItem={renderSongs}
+                data={songs}
+                keyExtractor={item => item.id}
+                horizontal
+                pagingEnabled
+                showsHorizontalScrollIndicator={false}
+                scrollEventThrottle={16}
+                onScroll={Animated.event(
+                  [
+                    {
+                      nativeEvent: {
+                        contentOffset: {x: scrollX},
+                      },
+                    },
+                  ],
+                  {useNativeDriver: true},
+                )}
+            />
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default RadioPlayer
